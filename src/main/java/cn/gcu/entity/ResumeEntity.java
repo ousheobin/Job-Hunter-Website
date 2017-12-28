4582dca73cd5b8e87fa172f2cb796bdf12f70331
Shaobin.Ou <me@oushaobin.cn>
2017-12-28 18:53:04 +0800

    增加简历使用用途字段

--- src/main/java/cn/gcu/entity/ResumeEntity.java
+++ src/main/java/cn/gcu/entity/ResumeEntity.java
@@ -23,6 +23,8 @@ public class ResumeEntity extends IdEntity {
 	private String gender;
 	@Column(name = "f_phone")
 	private String phone;
+	@Column(name = "f_apply_work")
+	private String applyWork;
 	@Column(name = "f_birthday")
 	private String birthday;
 	@Column(name = "f_time_to_work")
@@ -41,71 +43,101 @@ public class ResumeEntity extends IdEntity {
 	@Type(type = "text")
 	private String evaluate;
 	@ManyToOne
-	@JoinColumn(name="f_user")
+	@JoinColumn(name = "f_user")
 	private UserEntity user;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
+	public String getApplyWork() {
+		return applyWork;
+	}
+
+	public void setApplyWork(String applyWork) {
+		this.applyWork = applyWork;
+	}
+
 	public String getBirthday() {
 		return birthday;
 	}
+
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
+
 	public String getTimeToWork() {
 		return timeToWork;
 	}
+
 	public void setTimeToWork(String timeToWork) {
 		this.timeToWork = timeToWork;
 	}
+
 	public String getResidence() {
 		return residence;
 	}
+
 	public void setResidence(String residence) {
 		this.residence = residence;
 	}
+
 	public String getOrginResidence() {
 		return orginResidence;
 	}
+
 	public void setOrginResidence(String orginResidence) {
 		this.orginResidence = orginResidence;
 	}
+
 	public List<EducationEntity> getEducationDeatil() {
 		return educationDeatil;
 	}
+
 	public void setEducationDeatil(List<EducationEntity> educationDeatil) {
 		this.educationDeatil = educationDeatil;
 	}
+
 	public List<ExperienceEntity> getExperienceDetail() {
 		return experienceDetail;
 	}
+
 	public void setExperienceDetail(List<ExperienceEntity> experienceDetail) {
 		this.experienceDetail = experienceDetail;
 	}
+
 	public String getEvaluate() {
 		return evaluate;
 	}
+
 	public void setEvaluate(String evaluate) {
 		this.evaluate = evaluate;
 	}
+
 	public UserEntity getUser() {
 		return user;
 	}
+
 	public void setUser(UserEntity user) {
 		this.user = user;
 	}
